afdccfa5ec0ab4c6ea8050a06428c2bc98467bc2
hongsec <hongsec0506@naver.com>
2016-05-03 22:44:31 +0900

    整理

--- tianTianShuQian/src/main/java/com/izqisoft/view/TimeInterface.java
+++
@@ -1,9 +0,0 @@
-package com.izqisoft.view;
-
-public interface TimeInterface {
-
-	public void updateTime(int longTime);
-	public void updateMony(float mony);
-	public void gameOver(float mony);
-	
-}
